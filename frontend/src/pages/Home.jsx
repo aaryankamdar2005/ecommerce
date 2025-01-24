2bef57edf415a9a644cf9d87e9ab8a2b8294c447
aaryankamdar2005 <aaryankamdar2005@gmail.com>
2025-01-24 18:27:57 +0530

    final1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
-import LatestCollections from '../components/LatestCollections.jsx'
+import LatestCollections from '../components/Latestcollections.jsx'
 import BestSeller from '../components/BestSeller.jsx'
 import OurPolicy from '../components/OurPolicy.jsx'
 import Newsletter from '../components/Newsletter.jsx'
