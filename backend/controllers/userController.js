3a84f1b6b02451071ed11a6116b2d3ceca3efc7d
aaryankamdar2005 <aaryankamdar2005@gmail.com>
2025-01-26 02:56:58 +0530

    s

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose= require('mongoose')
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel.js');
